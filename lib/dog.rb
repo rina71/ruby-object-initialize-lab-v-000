10c627aa9f7674c820cde97f898e0103e3438027
Yurina Shin <yurinas@gmail.com>
2017-11-21 05:22:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,9 @@ class Dog
   def initialize(dog_name,dog_breed)
     @name = dog_name
     @breed = dog_breed
-  
+
     if !dog_breed
-      dog_breed == "Mutt"
+      dog_breed = "Mutt"
   end
 end
 end
